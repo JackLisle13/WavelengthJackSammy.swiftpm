1554bc3db8ad191c401c009cfc0fd18d80354d13
Sammy <sammy@kocentral.com>
2025-12-09 08:07:55 -0600

    Added GuessView

--- GuessView.swift
+++ GuessView.swift
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct PlayView: View {
+struct GuessView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
     }
 }
 
 #Preview {
-    PlayView()
+    GuessView()
 }
